015de73cec2277dad5ccf912bf5bb440c5c9664f
Sunil Bandla <sunilbandla@gmail.com>
2018-07-30 22:37:54 -0700

    Add link to feedseer source

--- lib/mastodon/version.rb
+++ lib/mastodon/version.rb
@@ -33,7 +33,7 @@ module Mastodon
     end
 
     def source_base_url
-      'https://github.com/tootsuite/mastodon'
+      'https://bitbucket.org/savyasachi1/feedseer'
     end
 
     # specify git tag or commit hash here
