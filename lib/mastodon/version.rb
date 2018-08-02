288807e4f002611132306166c7c37fd773718844
Sunil Bandla <sunilbandla@gmail.com>
2018-08-01 20:07:21 -0700

    Change repo url

--- lib/mastodon/version.rb
+++ lib/mastodon/version.rb
@@ -33,7 +33,7 @@ module Mastodon
     end
 
     def source_base_url
-      'https://github.com/sunilbandla/feedseer'
+      'https://github.com/sunilbandla/mastodon'
     end
 
     # specify git tag or commit hash here
